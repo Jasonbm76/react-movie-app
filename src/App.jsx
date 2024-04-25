eeebf82b17a1e07156cad73acf0395e4d7bd6124
Jason Murphy <jasonbm76@gmail.com>
2024-04-24 21:39:04 -0500

    Format movie grid and created addfavorite component

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,20 @@ import { React, useState, useEffect } from 'react';
 import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
+import AddFavorite from './components/AddFavorite';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import './App.scss';
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [searchValue, setSearchValue] = useState('s=star wars');
+  const [searchValue, setSearchValue] = useState('Star Wars');
 
   const urlBase = import.meta.env.VITE_OMDB_API_BASEURL;
   const apiKey = import.meta.env.VITE_OMDB_API_KEY;
 
-  const getMovieRequest = async () => {
-    const url = `${urlBase}/?${searchValue}&apikey=${apiKey}`;
+  const getMovieRequest = async (searchValue) => {
+    const url = `${urlBase}/?s=${searchValue}&apikey=${apiKey}`;
 
     const response = await fetch(url);
     const responseJson = await response.json();
@@ -27,12 +28,16 @@ function App() {
   };
 
   useEffect(() => {
-    getMovieRequest();
+    document.title = 'Movie Search App';
   }, []);
+
+  useEffect(() => {
+    getMovieRequest(searchValue);
+  }, [searchValue]);
 
   return (
     <div className="container-fluid movie-app">
-      <div className="row d-flex align-items-center mt-4 mb-4">
+      <div className="row d-flex mt-4 mb-4">
         <MovieListHeading heading="Movies" />
         <SearchBox
           searchValue={searchValue}
@@ -40,7 +45,10 @@ function App() {
         />
       </div>
       <div className="row">
-        <MovieList movies={movies} />
+        <MovieList
+          movies={movies}
+          favoriteComponent={AddFavorite}
+        />
       </div>
     </div>
   );
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 
 const MovieList = (props) => {
@@ -6,19 +5,61 @@ const MovieList = (props) => {
     movies: PropTypes.array.isRequired,
   };
 
-  return (
-    <>
-      {props.movies.map((movie) => (
+  const FavoriteComponent = props.favoriteComponent;
+
+  console.table(props.movies);
+
+  const filteredMovies = props.movies
+    .filter((movie) => {
+      return movie.Poster != 'N/A';
+    })
+    .sort(function (y) {
+      return y.Year;
+    })
+    .reverse()
+    .map((movie) => {
+      return (
         <div
           key={movie.imdbID}
-          className="col image-container d-flex justify-content-start m-3">
-          <img
-            src={movie.Poster}
-            alt="movie"></img>
+          className="col-auto image-container justify-content-start m-3">
+          <div className="image-container-inner">
+            <img
+              src={movie.Poster}
+              alt="movie"
+            />
+          </div>
+          <div className="overlay">
+            <FavoriteComponent />
+          </div>
+
+          <div className="movieTitle">
+            {movie.Title} ({movie.Year})
+          </div>
         </div>
-      ))}
-    </>
-  );
+      );
+    });
+
+  return filteredMovies;
+
+  // const movieMap = props.movies;
+
+  // //console.info(movieMap);
+
+  // return (
+  //   <>
+  //     {props.movies.map((movie) => (
+  //       <div
+  //         key={movie.imdbID}
+  //         className="col-auto m-3">
+  //         <img
+  //           src={movie.Poster}
+  //           alt="movie"
+  //         />
+  //         <div className="overlay d-flex align-items-center justify-content-center">Add to Favorites</div>
+  //       </div>
+  //     ))}
+  //   </>
+  // );
 };
 
 export default MovieList;
